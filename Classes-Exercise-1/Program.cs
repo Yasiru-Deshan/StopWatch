2a6032aa69ce331363ca256b8869b43e8717fc3b
yassa98 <yasirudeshan3@gmail.com>
2021-11-27 21:49:09 +0530

    first commit

---
+++ Classes-Exercise-1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Classes_Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            var duration = new TimeSpan();
+            var totDuration = new TimeSpan();
+
+            var stopWatch = new StopWatch();
+            Console.WriteLine("Type S to Start..");
+            var startString = Console.ReadLine();
+
+            while (i == 0)
+            {
+
+              
+
+                if (startString == "s")
+                {
+                    stopWatch.Start();
+                }
+                else
+                {
+                    Console.WriteLine("Type S to Start..");
+
+                }
+
+                Console.WriteLine("Type R to Stop");
+                var stopString = Console.ReadLine();
+                if (stopString == "r")
+                {
+                    stopWatch.Stop();
+
+                }else if(stopString == "s")
+                {
+                    stopWatch.Start();
+                }
+                else
+                {
+                    Console.WriteLine("Type R to Stop");
+                }
+
+                duration = stopWatch.stopTime - stopWatch.startTime;
+                Console.WriteLine("Duration {0}", duration);
+
+                totDuration = totDuration + duration;
+
+                Console.WriteLine("Type S to Start..");
+                var again = Console.ReadLine();
+
+                if(again == "s")
+                {
+                    i = 0;
+                }
+                else
+                {
+                    i = 1;
+                   
+                    Console.WriteLine("Total Duration : {0}", totDuration);
+                }
+            }
+
+        }
+    }
+}
---
+++ Classes-Exercise-1/StopWatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Exercise_1
+{
+    public class StopWatch
+    {
+        public DateTime startTime;
+        public DateTime stopTime;
+        public bool IsRunning;
+        public void Start()
+        {
+            if (!IsRunning)
+            {
+                IsRunning = true;
+                startTime = DateTime.Now;
+                Console.WriteLine("Start Time : {0} ", DateTime.Now.ToString("yyyyMMddHHmmss"));
+            }
+            else
+            {
+                throw new InvalidOperationException("Stopwatch is already running");
+            }
+        }
+
+        public void Stop()
+        {
+
+            if (IsRunning)
+            {
+                IsRunning = false;
+                stopTime = DateTime.Now;
+                Console.WriteLine("End Time : {0}", DateTime.Now.ToString("yyyyMMddHHmmss"));
+            }
+        }
+    }
+}
